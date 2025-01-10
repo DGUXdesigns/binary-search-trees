10ed83f5d8603a9bf5fbcaba0a22d56081a6cb9c
Daveian <140139328+DGUXdesigns@users.noreply.github.com>
2025-01-09 23:38:04 -0500

    Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,20 +4,20 @@ import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    languageOptions: {
-      globals: { ...globals.node, ...globals.browser },
-    },
-  },
-  pluginJs.configs.recommended,
-  eslintPluginPrettier,
-  {
-    rules: {
-      'prettier/prettier': 'error', // Treat Prettier formatting issues as errors
-      'capitalized-comments': ['error', 'always'],
-      eqeqeq: ['error', 'always'], // Enforce strict equality (===)
-      'no-console': 'warn', // Warn when console.log is used
-      'no-unused-vars': 'warn',
-    },
-  },
+	{
+		languageOptions: {
+			globals: { ...globals.node, ...globals.browser },
+		},
+	},
+	pluginJs.configs.recommended,
+	eslintPluginPrettier,
+	{
+		rules: {
+			'prettier/prettier': 'off', // Treat Prettier formatting issues as errors
+			'capitalized-comments': ['error', 'always'],
+			eqeqeq: ['error', 'always'], // Enforce strict equality (===)
+			'no-console': 'warn', // Warn when console.log is used
+			'no-unused-vars': 'warn',
+		},
+	},
 ];
